d70256f309f199b0cebd485ddfb713cc5ebea35f
maciejeicam <maciej_michalik@onet.pl>
2023-03-05 12:17:11 +0100

    Initial Commit

---
+++ Fiszki/FiszkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiszkiApp.swift
+//  Fiszki
+//
+//  Created by Maciej Michalik on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FiszkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
